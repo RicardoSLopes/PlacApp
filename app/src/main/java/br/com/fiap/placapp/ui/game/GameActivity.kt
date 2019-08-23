baac6c2e56256b56ab800ca5a941fd79b6c73bdb
Logon Aluno <logonaluno@local.com>
2019-08-22 22:36:30 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/placapp/ui/game/GameActivity.kt
@@ -0,0 +1,27 @@
+package br.com.fiap.placapp.ui.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.placapp.R
+import br.com.fiap.placapp.ui.game.event.EventFragment
+import kotlinx.android.synthetic.main.activity_game.*
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        showEventFragment()
+
+        ivBack.setOnClickListener{
+            onBackPressed()
+        }
+
+    }
+
+    private fun showEventFragment(){
+        val ft = supportFragmentManager.beginTransaction()
+        ft.add(R.id.containerGame, EventFragment())
+        ft.commit()
+    }
+}
---
+++ app/src/main/java/br/com/fiap/placapp/ui/score/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.placapp.ui.score
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
